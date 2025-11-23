4dc4c54588e14c18e08a4b0ef8581aff704f8e34
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-11-23 15:23:35 +0700

    Delete resource not use

--- globals/ap-southeast-1/terraform-state/main.tf
+++ globals/ap-southeast-1/terraform-state/main.tf
@@ -11,50 +11,13 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-# 1. Tạo S3 Bucket lưu state
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "luyenhaidangit-bucket-terraform-state-2211"
-
-  tags = {
-    Name        = "terraform-state"
-    Environment = "global"
-    Owner       = "luyenhaidangit"
-    Project     = "flex"
-  }
-}
-
-# 2. Versioning cho rollback state
-resource "aws_s3_bucket_versioning" "tf_state_versioning" {
-  bucket = aws_s3_bucket.tf_state.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# 3. Block mọi truy cập public
-resource "aws_s3_bucket_public_access_block" "tf_state_public" {
-  bucket = aws_s3_bucket.tf_state.id
-
-  block_public_acls       = true
-  ignore_public_acls      = true
-  block_public_policy     = true
-  restrict_public_buckets = true
-}
-
-# 4. Bật Server Side Encryption AES256
-resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_sse" {
-  bucket = aws_s3_bucket.tf_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
+# 1. Tạo s3 lưu  tf state
 resource "aws_s3_bucket" "terraform_state_bucket" {
   bucket = "flex-apse1-terraform-state"
+
+  lifecycle {
+    prevent_destroy = true
+  }
 
   tags = {
     Name        = "terraform-state"
@@ -95,24 +58,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_s
 }
 
 # 5. DynamoDB table dùng để lock Terraform state
-resource "aws_dynamodb_table" "tf_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name        = "terraform-locks"
-    Environment = "global"
-    Owner       = "luyenhaidangit"
-    Project     = "flex"
-  }
-}
-
 resource "aws_dynamodb_table" "terraform_state_locks" {
   name         = "terraform_state_locks"
   billing_mode = "PAY_PER_REQUEST"
