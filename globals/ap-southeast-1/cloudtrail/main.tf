083dc7ff4bd0bfd91e48faada518299a3bf52c9b
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-11-23 21:50:40 +0700

    Change structure cloudtrail

--- globals/ap-southeast-1/cloudtrail/main.tf
+++ globals/ap-southeast-1/cloudtrail/main.tf
@@ -1,18 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
-data "aws_caller_identity" "current" {}
-
 # 1. Log bucket cho CloudTrail
 resource "aws_s3_bucket" "cloudtrail_bucket" {
   bucket = "flex-apse1-cloudtrail-logs"
