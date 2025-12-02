f77cab5520d7730281f68d93a7d3398113691fee
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-12-02 18:50:53 +0700

    Add cloudwatch log for network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -279,3 +279,16 @@ resource "aws_vpc_endpoint" "interface" {
     { Name = "${var.name}-${each.key}-endpoint" }
   )
 }
+
+# CloudWatch logs
+resource "aws_cloudwatch_log_group" "vpc_flow" {
+  name              = "/aws/vpc/${var.name}-flow-logs"
+  retention_in_days = 30
+}
+
+resource "aws_flow_log" "vpc_flow" {
+  log_destination_type = "cloud-watch-logs"
+  log_group_name       = aws_cloudwatch_log_group.vpc_flow.name
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.this.id
+}
