99bd6def9cd267bd85bbd20c86e3daf525af061f
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-12-05 01:59:07 +0700

    Change cidr subnet network

--- live/dev/ap-southeast-1/network/main.tf
+++ live/dev/ap-southeast-1/network/main.tf
@@ -5,9 +5,9 @@ module "network" {
   vpc_cidr             = "10.10.0.0/16"
   az                   = "ap-southeast-1a"
 
-  public_subnet_cidr  = "10.0.1.0/24"
-  private_subnet_cidr = "10.0.11.0/24"
-  db_subnet_cidr      = "10.0.21.0/24"
+  public_subnet_cidr  = "10.10.1.0/24"
+  private_subnet_cidr = "10.10.11.0/24"
+  db_subnet_cidr      = "10.10.21.0/24"
 
   tags = {
     Name        = "network"
