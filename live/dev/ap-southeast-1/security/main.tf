4e30a180b735c755bf38e4af9f21bce935540cf0
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-12-05 13:25:44 +0700

    Update module security

--- live/dev/ap-southeast-1/security/main.tf
+++ live/dev/ap-southeast-1/security/main.tf
@@ -1,23 +1,16 @@
-# Lấy thông tin từ state của layer Network
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
-    bucket = "flex-apse1-terraform-state"
-    key    = "dev/ap-southeast-1/network/terraform.tfstate"
-    region = "ap-southeast-1"
-  }
-}
-
 module "sg" {
   source = "../../../../modules/security"
 
   name   = "flex"
-  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+  vpc_id = module.vpc.vpc_id
 
   alb_ingress_cidrs = ["0.0.0.0/0"]
-  
-  # Module security đã tự động link:
-  # ALB SG -> App SG (port 80, 8080)
-  # App SG -> DB SG (port 5432)
-  # Nên không cần truyền ID vào nữa
+
+  app_ingress_sg_ids = [
+    module.sg.alb_sg_id
+  ]
+
+  db_ingress_sg_ids = [
+    module.sg.app_sg_id
+  ]
 }
