4c5d87555f1c9a37d77bbbabccf33b3ffef1d599
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-12-05 14:43:51 +0700

    Get ouput when apply network

---
+++ live/dev/ap-southeast-1/network/outputs.tf
@@ -0,0 +1,16 @@
+output "vpc_id" {
+  value = module.network.vpc_id
+}
+
+output "public_subnet_id" {
+  value = module.network.public_subnet_id
+}
+
+output "private_subnet_id" {
+  value = module.network.private_subnet_id
+}
+
+output "db_subnet_id" {
+  value = module.network.db_subnet_id
+}
+
