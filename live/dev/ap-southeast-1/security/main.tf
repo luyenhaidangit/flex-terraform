33823ed3c2aa690d2db350e79ce4771c7633de7e
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-12-07 15:16:13 +0700

    Fix bug missing argument vpc

--- live/dev/ap-southeast-1/security/main.tf
+++ live/dev/ap-southeast-1/security/main.tf
@@ -1,8 +1,9 @@
 module "sg" {
   source = "../../../../modules/security"
 
-  name   = "flex"
-  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+  name     = "flex"
+  vpc_id   = data.terraform_remote_state.network.outputs.vpc_id
+  vpc_cidr = data.terraform_remote_state.network.outputs.vpc_cidr
 
   alb_ingress_cidrs = ["0.0.0.0/0"]
 }
