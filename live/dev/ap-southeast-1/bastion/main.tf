ce0cbbb79838df8ed0875e95c4f9f033859c9aaa
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-12-07 17:33:10 +0700

    Change position code bastion

--- live/dev/ap-southeast-1/bastion/main.tf
+++ live/dev/ap-southeast-1/bastion/main.tf
@@ -2,10 +2,14 @@ module "bastion" {
   source = "../../../../modules/bastion"
 
   name      = "flex"
-  subnet_id = data.terraform_remote_state.network.outputs.private_subnet_id
 
+  # Instance
   ami_id                     = "ami-093a7f5fbae13ff67"       # Amazon Linux 2023 AMI 2023.9.20251117.1 x86_64 HVM kernel-6.1
   instance_type              = "t2.micro"                    # t2.micro
+
+  # Network
+  subnet_id = data.terraform_remote_state.network.outputs.private_subnet_id
+
   enable_detailed_monitoring = false                         # Free tier eligible
   
   volume_size                = 8
