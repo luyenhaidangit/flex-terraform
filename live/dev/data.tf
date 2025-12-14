3ff2dd28a346a3a6b2d63e8da8b7ea2784095ba1
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-12-14 18:37:38 +0700

    Create new vpc module

--- live/dev/data.tf
+++ live/dev/data.tf
@@ -1,12 +1,3 @@
 ########################################
-# Remote State - Network
+# Data Sources
 ########################################
-
-data "terraform_remote_state" "network" {
-  backend = "s3"
-  config = {
-    bucket = "flex-apse1-terraform-state"
-    key    = "dev/ap-southeast-1/network/terraform.tfstate"
-    region = "ap-southeast-1"
-  }
-}
--- live/dev/security-groups.tf
+++ live/dev/security-groups.tf
@@ -6,5 +6,5 @@ module "cluster_security_groups" {
   source = "../../modules/security-groups/cluster"
 
   name   = "dev-flex"
-  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+  vpc_id = module.vpc.vpc_id
 }
