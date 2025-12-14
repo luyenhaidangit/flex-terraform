44b452b8ab4113a27c67eb119960d1aa7dc601fa
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-12-14 19:10:43 +0700

    Init private plan

--- live/dev/network.tf
+++ live/dev/network.tf
@@ -1,5 +1,5 @@
 ########################################
-# VPC - Dev Environment
+# VPC
 ########################################
 
 module "vpc" {
@@ -7,4 +7,26 @@ module "vpc" {
 
   name       = "dev-flex"
   cidr_block = "10.0.0.0/16"
+}
+
+########################################
+# Private Subnets
+########################################
+
+module "private_subnet_1a" {
+  source = "../../modules/subnets/private"
+
+  name              = "dev-flex"
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = "ap-southeast-1a"
+}
+
+module "private_subnet_1b" {
+  source = "../../modules/subnets/private"
+
+  name              = "dev-flex"
+  vpc_id            = module.vpc.vpc_id
+  cidr_block        = "10.0.12.0/24"
+  availability_zone = "ap-southeast-1b"
 }
