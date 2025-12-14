0bea62921ce3a954a4b28da8f44b21a80180a89f
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-12-14 21:50:35 +0700

    Import module sg

--- live/dev/security-groups.tf
+++ live/dev/security-groups.tf
@@ -1,9 +1,20 @@
 ########################################
-# EKS Cluster Security Groups - Dev Environment
+# EKS Cluster Security Groups
 ########################################
 
 module "cluster_security_groups" {
   source = "../../modules/security-groups/cluster"
+
+  name   = "dev-flex"
+  vpc_id = module.vpc.vpc_id
+}
+
+########################################
+# Bastion Security Group
+########################################
+
+module "bastion_security_group" {
+  source = "../../modules/security-groups/bastion"
 
   name   = "dev-flex"
   vpc_id = module.vpc.vpc_id
