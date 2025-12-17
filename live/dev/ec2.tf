b5597f3933db3d0391884c648eb96329ef824f25
Luyện Hải Đăng <luyenhaidangit@gmail.com>
2025-12-17 12:33:33 +0700

    EC2 to internet gw

--- live/dev/ec2.tf
+++ live/dev/ec2.tf
@@ -7,7 +7,7 @@ module "bastion" {
   source = "../../modules/ec2/bastion"
 
   name                = "dev-flex"
-  subnet_id           = module.private_subnet_1a.subnet_id
+  subnet_id           = module.public_subnet_1a.subnet_id
   security_group_id   = module.bastion_security_group.bastion_security_group_id
   instance_profile_name = module.bastion_iam.bastion_instance_profile_name
 
